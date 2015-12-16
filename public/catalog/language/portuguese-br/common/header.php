e10389c5e89738a4db9d233a3831a4d748cbeb38
fernandofmendes <fernando.mendes@webca.com.br>
2015-12-16 11:53:01 -0200

    traduces

--- public/catalog/language/portuguese-br/common/header.php
+++ public/catalog/language/portuguese-br/common/header.php
@@ -8,6 +8,6 @@ $_['text_search']         = 'Digite aqui para buscar algum produto...';
 $_['text_welcome']        = 'Seja bem vindo, visitante! Você pode <a href="%s">entrar</a> ou <a href="%s">criar uma conta</a>.';
 $_['text_logged']         = 'Você está logado como <a href="%s">%s</a> <b>(</b> <a href="%s">Sair</a> <b>)</b>';
 $_['text_account']        = 'Minha conta';
-$_['text_checkout']       = 'Finalizar pedido';
+$_['text_checkout']       = 'Finalizar orçamento';
 $_['tx_phone']       = 'Atendimento ';
 ?>
--- public/catalog/language/portuguese-br/module/cart.php
+++ public/catalog/language/portuguese-br/module/cart.php
@@ -1,12 +1,12 @@
 <?php
 // Heading 
-$_['heading_title'] = 'Carrinho';
+$_['heading_title'] = 'Orçamento';
 
 // Text
 $_['text_items']    = '%s item(s) - %s';
-$_['text_empty']    = 'Seu carrinho está vazio!';
-$_['text_cart']     = 'Ver carrinho';
-$_['text_checkout'] = 'Finalizar compra';
+$_['text_empty']    = 'Sua lista de orçamento está vazio!';
+$_['text_cart']     = 'Ver orçamento';
+$_['text_checkout'] = 'Finalizar orçamento';
 
 $_['text_payment_profile'] = 'Perfil de pagamento';
 
