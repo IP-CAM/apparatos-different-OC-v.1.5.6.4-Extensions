ccd763c70b07649a85836f5dd069adcf64fe03bd
fernandofmendes <fernando.mendes@webca.com.br>
2016-01-28 03:11:22 -0200

    textos

--- public/catalog/language/portuguese-br/checkout/success.php
+++ public/catalog/language/portuguese-br/checkout/success.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Seu pedido foi confirmado!';
+$_['heading_title'] = 'Seu orçamento foi confirmado!';
 
 // Text
 $_['text_customer']  = '<p>Seu pedido foi confirmado com sucesso!</p><p>Caso tenha criado uma conta você pode ver o histórico dos seus pedidos acessando sua "<a href="%s">Conta</a>" e clicando em "<a href="%s">Histórico de pedidos</a>".</p><p>Por favor, entre em contato conosco caso tenha dúvidas clicando <a href="%s">aqui</a>.</p><p>Obrigado por comprar em nossa loja!</p><br />';
