b7500ee6740a472a232f9b855a8c59bffcf11d5a
fernandofmendes <fernando.mendes@webca.com.br>
2016-01-28 03:23:04 -0200

    textos

--- public/admin/language/portuguese-br/mail/order.php
+++ public/admin/language/portuguese-br/mail/order.php
@@ -2,7 +2,7 @@
 // Text
 $_['text_subject']		= '%s - Orçamento atualizado %s';
 $_['text_order']		= 'Orçamento Nº:';
-$_['text_date_added']	= 'Data da compra:';
+$_['text_date_added']	= 'Data do orçamento:';
 $_['text_order_status']	= 'Seu orçamento foi atualizado para a seguinte situação:';
 $_['text_comment']		= 'Os comentários de seu orçamento são:';
 $_['text_link']			= 'Para ver o seu orçamento, por favor, clique no link abaixo:';
